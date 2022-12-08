f5a5f81760d9f7b7d6061932f35355096cba033b
Corey Verkouteren <90060114+clodon2@users.noreply.github.com>
2022-12-08 11:51:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import panda3d.core
+import ursina
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina.shaders import basic_lighting_shader
+
+app = Ursina()
+
+FOG_START = 10
+FOG_OPAQUE = 70
+
+
+random.seed(0)
+
+ground = Entity(model='cube', collider='box', scale=(64, 1, 64), texture='Textures/floor1.png', texture_scale=(4,4))
+tester = Entity(model='sphere', collider='box', scale=2, texture="grass")
+
+editor_camera = EditorCamera(enabled=False, ignore_paused=True)
+player = FirstPersonController(model='cube', z=10, color=color.orange, origin_y=-.5, speed=8)
+player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
+
+Sky(color=color.black)
+
+
+def input(key):
+    if key == "g":
+        print(scene.fog.getLinearOnsetPoint())
+        print(scene.fog.get_linear_onset_point())
+
+
+def update():
+    fog_start = (player.x + FOG_START, player.y + FOG_START, player.z + FOG_START)
+    fog_opaque = (player.x + FOG_OPAQUE, player.y + FOG_OPAQUE, player.z + FOG_OPAQUE)
+    player.camera_pivot.setFog(panda3d.core.Fog(1))
+
+
+def pause_input(key):
+    if key == "escape":
+        quit()
+    if key == 'tab':    # press tab to toggle edit/play mode
+        editor_camera.enabled = not editor_camera.enabled
+
+        player.visible_self = editor_camera.enabled
+        player.cursor.enabled = not editor_camera.enabled
+        mouse.locked = not editor_camera.enabled
+        editor_camera.position = player.position
+
+        application.paused = editor_camera.enabled
+
+
+pause_handler = Entity(ignore_paused=True, input=pause_input)
+
+app.run()
